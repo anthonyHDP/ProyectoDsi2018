1c7e185fc4d6e4535fbbcfb0a585b2ba93c56ea5
javierAleman09 <40651983+javierAleman09@users.noreply.github.com>
2018-06-29 09:54:11 -0600

    entrar a gestion de expediente de paciente

--- sisLog2/resources/views/clinica/index.blade.php
+++ sisLog2/resources/views/clinica/index.blade.php
@@ -8,13 +8,13 @@
             
         </div>
     </div>
-    <!--
+    
     <div class="row">
         <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
             <h3>Gestion de Medico <a href="clinica/medico"><button class="btn btn-success">Entrar</button></a></h3>
             
         </div>
     </div>
-    -->
+    
 
 @endsection
