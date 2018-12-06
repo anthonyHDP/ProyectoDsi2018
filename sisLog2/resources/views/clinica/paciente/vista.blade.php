e39bf7a91ef05eff4ed5d5dc9a2f4eb3dca9f25b
@ivanhenrygodoy <40482520+ivanhenrygodoy@users.noreply.github.com>
2018-12-06 00:37:56 -0600

    Vista de PDF de expediente de paciente
    Html para la vista del reporte de pacientes

--- sisLog2/resources/views/clinica/paciente/vista.blade.php
+++ sisLog2/resources/views/clinica/paciente/vista.blade.php
@@ -4,6 +4,14 @@
 <meta http-equiv="utf-8"/>
 <title>Expediente de Paciente</title>
 <style type="text/css">
+hr {
+          border-color: #66BDA9;
+          height: 1px;
+          margin: 5px 0;
+          display: block;
+          clear: both;
+      }
+.Estilo11 {font-weight: bold}
 <!--
 body {
 	background-image: url();
@@ -15,15 +23,24 @@ body {
 </style></head>
 
 <body>
-<p align="left">hola</p>
-<h1>Real Madrid</h1>
+
+
+<h1 align="center"><font color="#66BDA9">Clinica Medica Betel - Expediente de Paciente</font></h1>
+<hr>
+      
+      <div id="project">
+        <div><span>Direccion:</span> Barrio el Angel, Rosario de la Paz</div>
+        <div><span>Telefono:</span> 2531-2078</div>
+        <div><span>Horario: </span> Lunes a viernes de 12:30 pm a 3:30 pm </div>
+        <hr>
 
 <div align="left">
-  <table width="700" height="325" border="0" bordercolor="#F0F0F0" bgcolor="white">
+  <table width="540" height="230" border="0" bordercolor="#F0F0F0" bgcolor="white">
       <th height="42" background="
-" bgcolor="#FFFFFF" scope="col"><table width="700" height="250" border="0" align="center" bordercolor="#F0F0F0">
+" bgcolor="#FFFFFF" scope="col">
+<table width="540" height="230" border="0" align="center" bordercolor="#F0F0F0">
           <tr>
-            <td colspan="5" bgcolor="#999999" class="Estilo9">Expediente de Paciente</td>
+            <td colspan="5" bgcolor="skyblue" class="Estilo11">Expediente de Paciente</td>
           </tr>
           <tr>
             <th class="Estilo9" scope="col"><div align="left">Apellido:</div></th>
@@ -38,11 +55,11 @@ body {
             <td class="Estilo9"><div align="left"></div>{{$paciente->telefono}}</td>
           </tr>
           <tr>
-            <td class="Estilo9"><div align="left">Direccion</div></td>
+            <td class="Estilo9"><div align="left">Direccion:</div></td>
             <td class="Estilo9"><div align="left">{{$paciente->direccion}}</div></td>
           </tr>
           <tr>
-            <td class="Estilo9"><div align="left">Fecha Nacimiento</div></td>
+            <td class="Estilo9"><div align="left">Fecha Nacimiento:</div></td>
             <td class="Estilo9"><div align="left">{{$paciente->fechaNacimiento}}</div></td>
             </tr>
           <tr>
@@ -59,8 +76,17 @@ body {
             </tr>
         
           <tr>
-            <td colspan="5" bgcolor="#999999" class="Estilo9">&nbsp;</td>
+            <td colspan="5" bgcolor="skyblue" class="Estilo9">&nbsp;</td>
           </tr>
+          </table>
+          </th>
+          </table>
+
+          <hr>
+
+          <footer>
+          <center>Clama a mi y yo te respondere Jeremias 33:3.</center>         
+          </footer>
 </div>
 </body>
 </html>
