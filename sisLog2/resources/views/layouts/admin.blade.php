aa2fc77f4226925fac8bc45059fbc15d3f4c1680
aby <aby_viana@zoho.com>
2019-11-20 21:07:56 -0600

    cambio de color verde

--- sisLog2/resources/views/layouts/admin.blade.php
+++ sisLog2/resources/views/layouts/admin.blade.php
@@ -21,7 +21,7 @@
     <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
 
   </head>
-  <body class="hold-transition skin-blue sidebar-mini">
+  <body class="hold-transition skin-green-light sidebar-mini">
     <div class="wrapper">
 
       <header class="main-header">
